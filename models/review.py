d9d73660c0b6012f6b23228e22ac274e387905b6
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-08-08 01:44:46 +0100

    Review class

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Review class inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Child of BaseModels with
+    Public attributes:
+    place_id: string - empty string: it will be the Place.id
+    user_id: string - empty string: it will be the User.id
+    text: string - empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
